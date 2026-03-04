9de7d9831a72341fd9119ae12d4cf2dcd47e60bc
consulteplus <marketing@consulteplus.com.br>
2026-03-04 01:17:10 -0300

    fix: adiciona database.php nos acoes.php chamados via AJAX (sem header.php)

--- modules/crm/acoes.php
+++ modules/crm/acoes.php
@@ -1,6 +1,7 @@
 ﻿<?php
 // crm/acoes.php
 require_once __DIR__ . '/../../config/config.php';
+require_once __DIR__ . '/../../config/database.php';
 require_once __DIR__ . '/../../includes/auth.php';
 require_once __DIR__ . '/../../classes/CrmService.php';
 require_once __DIR__ . '/../../classes/SupabaseClient.php';
